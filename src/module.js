b1643778e62275d81ff8a13f1422b8be8eba9f57
milan gupta <milangupta511@gmail.com>
2021-08-29 22:27:56 +0530

    Initial commit

---
+++ src/module.js
@@ -0,0 +1,20 @@
+/**
+ * Module
+ */
+export default class Module {
+  constructor() {
+    let settings = {};
+
+    let selector = {};
+
+    let state = {};
+  }
+
+  init() {
+    console.log('Module.init()');
+
+    this.bindEvents();
+  }
+
+  bindEvents() {}
+}
